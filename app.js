503d5910b82ea0a393711733620dc82119b1ce56
Kapil Shrestha <contact@kapilshrestha.com>
2017-09-08 17:33:19 +1200

    Mongo db LibraryApp

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ var app = express();
 
 var port = process.env.PORT || 5000;
 var nav = [{
-    Link:'/Books',
-    Text:'Book'
-   },{
-    Link:'/Authors',
-    Text:'Author'
-   }];
-var bookRouter =require('./src/routes/bookRoutes')(nav);
+    Link: '/Books',
+    Text: 'Book'
+}, {
+    Link: '/Authors',
+    Text: 'Author'
+}];
+var bookRouter = require('./src/routes/bookRoutes')(nav);
+var adminRouter = require('./src/routes/adminRoutes')(nav);
 
 app.use(express.static('public'));
 app.set('views', './src/views');
@@ -18,6 +19,7 @@ app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 app.use('/Books', bookRouter);
+app.use('/Admin', adminRouter);
 
 app.get('/', function (req, res) {
     res.render('index', {
